ab94d7896e16212673293d3652c280ed3f4941d0
SvetlanaS-72 <146142217+SvetlanaS-72@users.noreply.github.com>
2024-05-26 15:27:09 +0300

    feat: added frontend URL to cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,6 @@
 const allowedCors = [ "http://localhost:3000", 
-                      "http://localhost:3001", ]
-                     // "http://"           ]
+                      "http://localhost:3001", 
+                      "https://student-svets.nomoredomainswork.ru"]
 
 function cors(req, res, next) {
     const {origin } = req.headers;
